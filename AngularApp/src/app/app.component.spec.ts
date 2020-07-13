5c839be27627d5405cc7f4ff17c539ae183a125a
pzamora <pzamora@vectoritcgroup.com>
2020-07-13 00:48:16 -0500

    Estructura

--- AngularApp/src/app/app.component.spec.ts
+++ AngularApp/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularPrimeraApp'`, () => {
+  it(`should have as title 'AngularApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularPrimeraApp');
+    expect(app.title).toEqual('AngularApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularPrimeraApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AngularApp app is running!');
   });
 });
